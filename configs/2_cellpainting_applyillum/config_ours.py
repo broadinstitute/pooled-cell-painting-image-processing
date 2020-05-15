2763ce6a76cdbf35f15bb6d7940d0f573062a03a
Beth Cimini <beth.cimini@gmail.com>
2020-05-15 17:41:00 -0400

    add step 2 configs, edit step1

--- configs/2_cellpainting_applyillum/config_ours.py
+++ configs/2_cellpainting_applyillum/config_ours.py
@@ -1,6 +1,6 @@
 # Constants (User configurable)
 
-APP_NAME = '2018_11_20_Periscope_X_IllumPainting'                # Used to generate derivative names unique to the application.
+APP_NAME = '2018_11_20_Periscope_X_ApplyIllumPainting'                # Used to generate derivative names unique to the application.
 
 # DOCKER REGISTRY INFORMATION:
 DOCKERHUB_TAG = 'bethcimini/distributed-cellprofiler:1.2.1_319_highmem_plugins'
@@ -17,7 +17,7 @@ CLUSTER_MACHINES = 1
 TASKS_PER_MACHINE = 1
 MACHINE_TYPE = ['m4.xlarge']
 MACHINE_PRICE = 0.10
-EBS_VOL_SIZE = 300                       # In GB.  Minimum allowed is 22.
+EBS_VOL_SIZE = 350                       # In GB.  Minimum allowed is 22.
 
 # DOCKER INSTANCE RUNNING ENVIRONMENT:
 DOCKER_CORES = 4                        # Number of CellProfiler processes to run inside a docker container
@@ -29,13 +29,13 @@ SECONDS_TO_START = 3*60                 # Wait before the next CP process is ini
 
 #SQS QUEUE INFORMATION:
 SQS_QUEUE_NAME = APP_NAME + 'Queue'
-SQS_MESSAGE_VISIBILITY = 720*60           # Timeout (secs) for messages in flight (average time to be processed)
+SQS_MESSAGE_VISIBILITY = 120*60           # Timeout (secs) for messages in flight (average time to be processed)
 SQS_DEAD_LETTER_QUEUE = 'arn:aws:sqs:us-east-1:XXXXXXXXXXXX:DeadMessages'
 
 # LOG GROUP INFORMATION:
 LOG_GROUP_NAME = APP_NAME
 
 # REDUNDANCY CHECKS
-CHECK_IF_DONE_BOOL = 'True'  #True or False- should it check if there are a certain number of non-empty files and delete the job if yes?
+CHECK_IF_DONE_BOOL = 'False'  #True or False- should it check if there are a certain number of non-empty files and delete the job if yes?
 EXPECTED_NUMBER_FILES = 5    #What is the number of files that trigger skipping a job?
 MIN_FILE_SIZE_BYTES = 1
