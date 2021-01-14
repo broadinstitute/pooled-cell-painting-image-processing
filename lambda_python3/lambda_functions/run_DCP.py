68bc10bcfdb49b593ba874f3326d3b185318a986
ErinWeisbart <eweisbar@broadinstitute.org>
2021-01-14 09:38:40 -0800

    helpful error message if config files missing

--- lambda_python3/lambda_functions/run_DCP.py
+++ lambda_python3/lambda_functions/run_DCP.py
@@ -1,10 +1,8 @@
 import os
 import sys
-
 import boto3
 
 sys.path.append("/tmp")
-
 
 def run_setup(bucket_name, prefix, batch, step, cellprofiler=True):
     os.chdir("/tmp")
@@ -20,31 +18,28 @@ def run_setup(bucket_name, prefix, batch, step, cellprofiler=True):
         sys.modules.pop("config_ours")
         print("popped previous config_ours")
     import boto3_setup
-
     app_name = boto3_setup.setup(cellprofiler=cellprofiler)
     return app_name
-
 
 def run_cluster(bucket_name, prefix, batch, step, filename, njobs):
     os.chdir("/tmp")
     grab_fleet_file(bucket_name, prefix, batch, step, filename)
     import boto3_setup
-
     boto3_setup.startCluster("fleet_ours.json", njobs)
-
 
 def run_monitor(bucket_name, prefix, batch, step):
     import boto3_setup
-
     boto3_setup.upload_monitor(bucket_name, prefix, batch, step)
-
 
 def grab_batch_config(bucket_name, prefix, batch, step):
     s3 = boto3.client("s3")
     our_config = prefix + "lambda/" + batch + "/" + str(step) + "/config_ours.py"
-    with open("/tmp/config_ours.py", "wb") as f:
-        s3.download_fileobj(bucket_name, our_config, f)
-
+    try:
+        with open("/tmp/config_ours.py", "wb") as f:
+            s3.download_fileobj(bucket_name, our_config, f)
+    except ClientError:
+        print ("Config files for this batch haven't been uploaded to S3.")
+        return
 
 def grab_fleet_file(bucket_name, prefix, batch, step, filename):
     s3 = boto3.client("s3")
