53f9bc8c006afe007d94d515dfa185a77bc2dd20
ErinWeisbart <54687786+ErinWeisbart@users.noreply.github.com>
2025-06-26 08:28:51 -0700

    debug

--- FIJI/Batch_StitchByRow_Headless.py
+++ FIJI/Batch_StitchByRow_Headless.py
@@ -29,7 +29,7 @@ import os
 
 row_widths = [5,7,7,7,7,7,5]
 imagesize = 2960
-tilesize = 1480*float(scalingstring) # barcoding acquisition size * scaling string
+tilesize = 2960*float(scalingstring) # barcoding acquisition size * scaling string
 
 top_outfolder = 'output'
 if not os.path.exists(top_outfolder):
@@ -48,7 +48,7 @@ if not os.path.exists(out_subdir):
         os.mkdir(out_subdir)
 if not os.path.exists(tile_subdir):
         os.mkdir(tile_subdir)
-		
+
 subdir=os.path.join(input_file_location,subdir)
 
 if awsdownload == 'True':
@@ -110,6 +110,7 @@ if os.path.isdir(subdir):
         for presuf in presuflist:
                 if channame in presuf[1]:
                         firstpresuf = presuf
+                        firstprefix, firstsuffix = presuf
         # make sure channame is processed first
         presuflist.remove(firstpresuf)
         presuflist.insert(0, firstpresuf)
@@ -142,22 +143,21 @@ if os.path.isdir(subdir):
                                         standard_grid_instructions=["type=[Grid: snake by rows] order=["+order+"] grid_size_x="+str(rowlen)+" grid_size_y=1 tile_overlap="+str(overlap_pct)+" first_file_index_i="+str(startnum)+" directory="+subdir+" file_names=",
                                         " output_textfile_name=TileConfiguration_Row"+str(rownum)+".txt fusion_method=[Linear Blending] regression_threshold=0.30 max/avg_displacement_threshold=2.50 absolute_displacement_threshold=3.50 compute_overlap computation_parameters=[Save computation time (but use more RAM)] image_output=[Fuse and display]"]
                                         IJ.run("Grid/Collection stitching", standard_grid_instructions[0] + filename + standard_grid_instructions[1])
-                                        im=IJ.getImage()
                                         startnum += rowlen
                                 else:
                                         # use the stitching from the first channel
-                                        with open(os.path.join(out_subdir, "TileConfiguration_Row"+str(rownum)+".registered.txt"),'r') as infile:
-                                                with open(os.path.join(out_subdir, "TileConfiguration_Row"+str(rownum)+".registered_copy.txt"),'w') as outfile:
+                                        with open(os.path.join(subdir, "TileConfiguration_Row"+str(rownum)+".registered.txt"),'r') as infile:
+                                                with open(os.path.join(subdir, "TileConfiguration_Row"+str(rownum)+".registered_copy.txt"),'w') as outfile:
                                                         for line in infile:
-                                                                line=line.replace(firstpresuf,eachpresuf)
+                                                                line=line.replace(firstsuffix,thissuffix)
                                                                 outfile.write(line)
                                         copy_grid_instructions="type=[Positions from file] order=[Defined by TileConfiguration] directory="+subdir+" layout_file=TileConfiguration_Row"+str(rownum)+".registered_copy.txt fusion_method=[Linear Blending] regression_threshold=0.30 max/avg_displacement_threshold=2.50 absolute_displacement_threshold=3.50 ignore_z_stage computation_parameters=[Save computation time (but use more RAM)] image_output=[Fuse and display]"
                                         IJ.run("Grid/Collection stitching", copy_grid_instructions)
-                                        # make width consistent post-stitching
-                                        im=IJ.getImage()
-                                        height = im.getHeight()
-                                        width = imagesize*max(row_widths)
-                                        IJ.run("Canvas Size...", "width="+str(width)+" height="+str(height)+" position=Center zero")
+                                # make width consistent post-stitching
+                                im=IJ.getImage()
+                                height = im.getHeight()
+                                width = imagesize*max(row_widths)
+                                IJ.run("Canvas Size...", "width="+str(width)+" height="+str(height)+" position=Center zero")
                                 
                                 # check that it didn't have gross error
                                 height = im.getHeight()
@@ -179,7 +179,7 @@ if os.path.isdir(subdir):
                                 with open(os.path.join(out_subdir, "TileConfiguration.registered.txt"),'r') as infile:
                                         with open(os.path.join(out_subdir, "TileConfiguration.registered_copy.txt"),'w') as outfile:
                                                 for line in infile:
-                                                        line=line.replace(firstpresuf,eachpresuf)
+                                                        line=line.replace(firstsuffix,thissuffix)
                                                         outfile.write(line)
                                 copy_grid_instructions="type=[Positions from file] order=[Defined by TileConfiguration] directory="+out_subdir+" layout_file=TileConfiguration.registered_copy.txt fusion_method=[Linear Blending] regression_threshold=0.30 max/avg_displacement_threshold=2.50 absolute_displacement_threshold=3.50 ignore_z_stage computation_parameters=[Save computation time (but use more RAM)] image_output=[Fuse and display]"
                                 IJ.run("Grid/Collection stitching", copy_grid_instructions)
@@ -199,6 +199,7 @@ if os.path.isdir(subdir):
                         # hardcoded for 9 images per well, starting lower right, snake
                         # 0 counting to match non-stitch-crop barcoding
                         # IJ.makeRectangle starts at upper left
+                        print ("starting crop")
                         center = (imagesize*max(row_widths))/2
                         im=IJ.getImage()
                         # center (4)
@@ -243,26 +244,27 @@ if os.path.isdir(subdir):
                         # then crop each matched into 4 because the whole size is too large to run nicely in CellProfiler
                         for image in os.listdir(tile_subdir):
                                 if 'BCMatch' in image:
-                                        IJ.open(os.path.join(tile_subdir,image))
-                                        im=IJ.getImage()
+                                        if thissuffix in image:
+                                                IJ.open(os.path.join(tile_subdir,image))
+                                                im=IJ.getImage()
 
-                                        # make crops, upper left, snake
-                                        IJ.makeRectangle(0, 0,tilesize/2,tilesize/2)
-                                        im_tile=im.crop()
-                                        IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop0.tiff")))
-                                        
-                                        IJ.makeRectangle(tilesize/2, 0,tilesize/2,tilesize/2)
-                                        im_tile=im.crop()
-                                        IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop1.tiff")))
-                                        
-                                        IJ.makeRectangle(tilesize/2, tilesize/2,tilesize/2,tilesize/2)
-                                        im_tile=im.crop()
-                                        IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop2.tiff")))
-                                        
-                                        IJ.makeRectangle(0, tilesize/2,tilesize/2,tilesize/2)
-                                        im_tile=im.crop()
-                                        IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop3.tiff")))
-                                        
-                                        IJ.run("Close All")
+                                                # make crops, upper left, snake
+                                                IJ.makeRectangle(0, 0,tilesize/2,tilesize/2)
+                                                im_tile=im.crop()
+                                                IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop0.tiff")))
+                                                
+                                                IJ.makeRectangle(tilesize/2, 0,tilesize/2,tilesize/2)
+                                                im_tile=im.crop()
+                                                IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop1.tiff")))
+                                                
+                                                IJ.makeRectangle(tilesize/2, tilesize/2,tilesize/2,tilesize/2)
+                                                im_tile=im.crop()
+                                                IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop2.tiff")))
+                                                
+                                                IJ.makeRectangle(0, tilesize/2,tilesize/2,tilesize/2)
+                                                im_tile=im.crop()
+                                                IJ.saveAs(im_tile,"tiff", os.path.join(tile_subdir,image.replace('.tiff',"_Crop3.tiff")))
+                                                
+                                                IJ.run("Close All")
                         firstchan = False
         print("done with well "+well)
